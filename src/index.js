8ad287baf67e98706b74f6807083541ebd10b86e
Alesia_Sergeeva <alesiasiarheeva@gmail.com>
2020-10-25 23:44:09 +0300

    feat; solve the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2) return false;
+    let arr = [];
+    let regexp;
+    let screen = "[]\^$.|?*+()"
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (screen.includes(bracketsConfig[i][0])) {
+            arr.push(`\\${bracketsConfig[i][0]}\\${bracketsConfig[i][1]}`);
+        } else {
+            arr.push(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`);
+        }
+        regexp = new RegExp(arr.join("|"));
+    }
+    while (regexp.test(str)) {
+        str = str.replace(regexp, "");
+    }
+    return str.length === 0;
 }
